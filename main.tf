6b793c27b6ece8d0a17d6b2b6661d90b88b8aee7
Saiteja7120 <saitejavempatapu11@gmail.com>
2025-11-01 11:28:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ id = var.vpc_id
  
 resource "aws_subnet" "example" {
 vpc_id = data.aws_vpc.selected.id
-availability_zone = "us-east-1a"
+availability_zone = "eu-north-1a"
 cidr_block = cidrsubnet(data.aws_vpc.selected.cidr_block, 4, 1)
  
 tags = {
